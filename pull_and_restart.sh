dc516101fe12e4d44dd63ba325a00d8a63dee7e4
unknown <arnaud.boulay@epitech.eu>
2020-06-10 00:05:19 +0200

    Factorise scripts

--- pull_and_restart.sh
+++ pull_and_restart.sh
@@ -1,4 +1,6 @@
 git pull
 sudo docker container stop `sudo docker ps -a -q  --filter ancestor=tarkov-helper_crawler`
+sudo docker container stop `sudo docker ps -a -q  --filter ancestor=tarkov-helper_bot`
 sudo docker container prune -f
+sudo docker-compose run -d bot
 sudo docker-compose run -d crawler
--- pull_and_restart_bot.sh
+++
@@ -1,4 +0,0 @@
-git pull
-sudo docker container stop `sudo docker ps -a -q  --filter ancestor=tarkov-helper_bot`
-sudo docker container prune -f
-sudo docker-compose run -d bot
